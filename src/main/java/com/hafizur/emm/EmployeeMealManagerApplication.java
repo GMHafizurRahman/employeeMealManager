ab8a7280d822d848383eacb8ca42d36edb89d594
GMHafizurRahman <apon.mgt.iu@gmail.com>
2023-06-09 21:54:23 +0600

    Init commit

---
+++ src/main/java/com/hafizur/emm/EmployeeMealManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hafizur.emm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMealManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMealManagerApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/hafizur/emm/EmployeeMealManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafizur.emm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMealManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
